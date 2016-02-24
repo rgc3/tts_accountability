a5b29d4375d2e217b0cd6ffa10c2548a03d788cb
Robert Cunningham <rgc3dev@gmail.com>
2016-02-24 14:22:21 -0500

    remove migration with extra bio for user

--- db/migrate/20160223235854_add_details_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string
-    add_column :users, :location, :string
-    add_column :users, :bio, :text
-  end
-end
