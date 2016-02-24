6da45d5604a7debb271d0746342b5e366857f51a
Marty Kowbel <mekobes44@yahoo.com>
2016-02-24 12:37:09 -0500

    edit page update

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>', navpic: '50X50' }, default_url: '/images/:style/missing.png'
 
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   has_many :goals
   has_many :relationships
   has_many :friends, through: :relationships
