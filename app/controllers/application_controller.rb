2eca37e914537ad3e2ef631e565b10d42aeec556
Robert Cunningham <rgc3dev@gmail.com>
2016-02-25 07:57:34 -0500

    add paperclip avatar migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
+
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -12,12 +12,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit({ roles: [] }, :email, :password, :password_confirmation,
-               :name, :avatar, :bio)
+               :name, :avatar, :bio, :username, :location)
     end
 
     devise_parameter_sanitizer.for(:account_update) do |u|
       u.permit(:email, :password, :password_confirmation,
-               :current_password, :name, :avatar, :bio)
+               :current_password, :name, :avatar, :bio, :username, :location)
     end
   end
 end
