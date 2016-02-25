674cdd9550d0d29c1953c18c70083981a76ab711
Robert Cunningham <rgc3dev@gmail.com>
2016-02-25 15:43:32 -0500

    add name to sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160225124944) do
+ActiveRecord::Schema.define(version: 20160225125010) do
 
   create_table "goals", force: :cascade do |t|
     t.string   "name"
@@ -47,7 +46,6 @@ ActiveRecord::Schema.define(version: 20160225124944) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.date     "birthday"
-    t.text     "bio"
     t.string   "username"
     t.string   "location"
     t.string   "avatar_file_name"
